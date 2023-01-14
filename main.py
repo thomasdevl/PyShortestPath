e321c2a64873f8de5016cdab6f3cd399df81fa50
thomasdevl <thomas.devlamminck@student.uclouvain.be>
2023-01-15 00:00:10 +0100

    A* but it only goes to the bottom right
    needs fixing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from bfs import bfs
+import heapq
 
 def create_grid(rows, columns, tile_size, gap_size, x, y):
     grid = []
@@ -20,6 +20,7 @@ def clear_grid(grid):
             if column == 0 and row == 0:
                 continue
             grid[row][column] = (x, y, 0)
+    grid[0][0] = (grid[0][0][0], grid[0][0][1], 4)
     return grid
 
 
@@ -157,6 +158,65 @@ def depth_first_search(grid, tile_size, gap_size, screen):
 
 
 
+def heuristic(a, b):
+    # Manhattan distance as the heuristic function
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def a_star(grid, tile_size, gap_size, screen):
+    start_x, start_y = 0, 0 # find the start position
+    goal_x, goal_y = len(grid) - 1, len(grid[0]) - 1 # find the goal position
+    heap = [(0, (start_x, start_y))]
+    visited = set()
+    parent = {}
+    g_score = {(start_x, start_y): 0}
+    f_score = {(start_x, start_y): heuristic((start_x, start_y), (goal_x, goal_y))}
+    path = []
+    goal_reached = False
+    while heap:
+        current = heapq.heappop(heap)[1]
+        if current in visited:
+            continue
+        visited.add(current)
+        x, y = current
+
+        if current == (goal_x, goal_y):
+            goal_reached = True
+            break
+
+        grid[x][y] = (grid[x][y][0], grid[x][y][1], 6)
+        draw_grid(grid, tile_size, gap_size, screen)
+        pygame.display.update()
+        pygame.time.wait(100)
+
+        for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+            if 0 <= x+dx < len(grid) and 0 <= y+dy < len(grid[0]):
+                if grid[x+dx][y+dy][2] != 1:
+                    neighbor = (x+dx, y+dy)
+                    if neighbor in visited:
+                        continue
+                    tentative_g_score = g_score[current] + 1
+                    if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                        parent[neighbor] = current
+                        g_score[neighbor] = tentative_g_score
+                        f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, (goal_x, goal_y))
+                        heapq.heappush(heap, (f_score[neighbor], neighbor))
+        grid[x][y] = (grid[x][y][0], grid[x][y][1], 7)
+        draw_grid(grid, tile_size, gap_size, screen)
+        pygame.display.update()
+
+    if goal_reached:
+        current = (goal_x, goal_y)
+        while current != (start_x, start_y):
+            path.append(current)
+            current = parent[current]
+        path.append((start_x, start_y))
+        for pos in path[::-1]:
+            x, y = pos
+            grid[x][y] = (grid[x][y][0], grid[x][y][1], 5)
+        draw_grid(grid,tile_size, gap_size, screen)
+        pygame.display.update()
+    return goal_reached
+
 
 
 
@@ -173,6 +233,8 @@ def create_button(text, x, y, width, height, inactive_color, active_color, scree
                 breath_first_search(grid, tile_size, gap_size, screen )
             elif text == "DFS":
                 depth_first_search(grid, tile_size, gap_size, screen )
+            elif text == "A*":
+                a_star(grid, tile_size, gap_size, screen )
     else:
         pygame.draw.rect(screen, inactive_color, (x, y, width, height))
 
@@ -185,12 +247,12 @@ def create_button(text, x, y, width, height, inactive_color, active_color, scree
 def main():
     # Initialize pygame and create a window
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((1280,720))
     pygame.display.set_caption("Python Pathfinding")
     clock = pygame.time.Clock()
     
     # Create a grid
-    grid = create_grid(5, 5, 100, 5, 50, 70)
+    grid = create_grid(6, 8, 100, 5, 50, 70)
 
     
     # Create a loop to handle events
@@ -214,6 +276,7 @@ def main():
             create_button("CLEAR", 50, 10, 100, 50, (255, 0, 0), (128, 21, 43), screen, grid, 100, 5)
             create_button("BFS", 160, 10, 100, 50, (0, 0, 255), (15, 27, 131), screen, grid, 100, 5)
             create_button("DFS", 270, 10, 100, 50, (0, 0, 255), (15, 27, 131), screen, grid, 100, 5)
+            create_button("A*", 380, 10, 100, 50, (0, 0, 255), (15, 27, 131), screen, grid, 100, 5)
 
             # Update the display
             pygame.display.flip()
